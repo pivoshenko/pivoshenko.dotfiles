53015f2ee6373d008656008c0b952002896ce3e8
pivoshenko <pivoshenko@users.noreply.github.com>
2026-05-25 21:22:15 +0100

    chore(theme): sync lightened popil + add morok flavor
    - popil: Claude-grey base #1f1f1e, softened neutrals, k9s logo/border fix
    - add morok flavor theme files across all tools

--- dotfiles/.config/fish/themes/popil.fish
+++ dotfiles/.config/fish/themes/popil.fish
@@ -1,6 +1,6 @@
 set -Ux FZF_POPIL "\
---color=bg+:#231e1a,bg:#1c1917,spinner:#e2cfc6,hl:#c98787 \
---color=fg:#e7e5e4,header:#c98787,info:#a78cc4,pointer:#e2cfc6 \
---color=marker:#9faece,fg+:#e7e5e4,prompt:#a78cc4,hl+:#c98787 \
---color=selected-bg:#2c2622 \
---color=border:#57534e,label:#e7e5e4"
+--color=bg+:#262625,bg:#1f1f1e,spinner:#e2cfc6,hl:#c98787 \
+--color=fg:#e4e2de,header:#c98787,info:#a78cc4,pointer:#e2cfc6 \
+--color=marker:#9faece,fg+:#e4e2de,prompt:#a78cc4,hl+:#c98787 \
+--color=selected-bg:#2e2e2c \
+--color=border:#57534e,label:#e4e2de"
