31069957e0ed529a6618186217173e820bc0bb16
pivoshenko <pivoshenko@users.noreply.github.com>
2026-09-13 09:16:07 +0100

    feat(fish): add the cargo bin directory to PATH

--- dotfiles/.config/fish/exports.fish
+++ dotfiles/.config/fish/exports.fish
@@ -10,5 +10,6 @@ set -Ux HOMEBREW_NO_ANALYTICS 1
 set -Ux PYTHONBREAKPOINT IPython.terminal.debugger.set_trace
 
 # == Path ==
+fish_add_path -g $HOME/.cargo/bin
 fish_add_path -g $HOME/.docker/bin
 fish_add_path -g /opt/homebrew/opt/rustup/bin
